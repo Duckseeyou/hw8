a649e4a235ac4e893e94fad3c194a4c6e91df4d1
Duckseeyou <eborcubaev@gmail.com>
2024-09-29 22:09:40 +0600

    Initial commit

---
+++ app/src/main/java/com/m2/hw8/MainActivity.java
@@ -0,0 +1,27 @@
+package com.m2.hw8;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewTreeObserver;
+
+import androidx.activity.ComponentActivity;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends ComponentActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+}
